7fcd202bf215feb43d3779c72ad3ae74ca665d0c
Koala <koalakeno@gmail.com>
2023-09-05 21:56:44 -0300

    calculo de media

---
+++ app.js
@@ -0,0 +1,22 @@
+// comando de saída de dados
+/*
+alert("Oi, isso é uma mensagem")
+
+console.log("Olá, isso é outra mensagem")
+
+alert(10/5)
+
+console.log(10/5)
+*/
+
+nota1 = 10
+nota2 = 8
+nota3 = 5
+nota4 = 8
+
+console.log("A média do João é: ")
+console.log((nota1 + nota2 + nota3 + nota4)/4)
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log(media)
